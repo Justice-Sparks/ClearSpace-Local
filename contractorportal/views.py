2415e3214b50e83a13b0fc8a88a7993bba26a7be
Justice Sparks <sparksjustice664@gmail.com>
2026-02-12 12:52:30 -0700

    login/user/portal foundations

--- contractorportal/views.py
+++ contractorportal/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def contractor_login(request):
-    return render(request, "contractorportal/contractor_login.html")
+    return render(request, "registration/login.html")
+
+def portal_redirect(request):
+    return render(request, "registration/portal_redirect.html")
 
 @login_required
 def dashboard(request):
